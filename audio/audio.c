7cd7875724bc9e02c1f5cd8283af35be54b10a42
Redecorating <redecorating@protonmail.com>
2022-08-29 22:36:52 +1000

    change many info logs to debug
    reduces spam in the journal, which saves disk space (or if you set a
    limit on journal size, it means less of that limited space will be
    filled with these debug messages)

--- audio/audio.c
+++ audio/audio.c
@@ -600,7 +600,7 @@ void aaudio_handle_cmd_timestamp(struct aaudio_device *a, struct aaudio_msg *msg
     struct aaudio_subdevice *sdev;
     u64 devid, timestamp, update_seed;
     aaudio_msg_read_update_timestamp(msg, &devid, &timestamp, &update_seed);
-    dev_info(a->dev, "Received timestamp update for dev=%llx ts=%llx seed=%llx\n", devid, timestamp, update_seed);
+    dev_dbg(a->dev, "Received timestamp update for dev=%llx ts=%llx seed=%llx\n", devid, timestamp, update_seed);
 
     sdev = aaudio_find_dev_by_dev_id(a, devid);
     aaudio_handle_timestamp(sdev, time_os, timestamp);
